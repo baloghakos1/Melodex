2c3b9b2be5acacc694005311d3836be7ce68072b
3111TAN-17 <3111TAN-17@tanuzem.boronkay.local>
2025-09-11 11:17:48 +0200

    song seeder finished

--- resources/views/artists/index.blade.php
+++ resources/views/artists/index.blade.php
@@ -5,17 +5,34 @@
     <title>Artists</title>
 </head>
 <body>
-    <h1>All Artists</h1>
-
-    @if($artists->isEmpty())
-        <p>No artists found.</p>
-    @else
-        <ul>
-            @foreach($artists as $artist)
-                <li>{{ $artist->name }}</li>
-            @endforeach
-        </ul>
-    @endif
-
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <form method="GET" action="{{ request()->url() }}">
+                        {{ __('Válassz adattáblát: ') }}
+                        <select name="crud" id="crud" title="Adattábla" onchange="location = this.value">
+                        <option value="{{ route('artists.index') }}" selected>-- Adattáblák --</option>
+                            <option value="{{ route('artists.index') }}" >
+                                    
+                            </option>
+                            <option value="{{ route('artists.index') }}">
+                                    Tantárgyak
+                            </option>
+                            <option value="{{ route('artists.index') }}">
+                                    Osztályok
+                            </option>
+                            <option value="{{ route('artists.index') }}">
+                                    Osztályok_Tantárgyai
+                            </option>
+                            <option value="{{ route('artists.index') }}">
+                                    Osztályzatok
+                            </option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
