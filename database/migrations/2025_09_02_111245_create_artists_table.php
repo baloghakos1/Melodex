5eb7220332e495caba6ee00e485e415be4e78b36
3041TAN-16 <3041TAN-16@tanuzem.boronkay.local>
2025-09-23 14:07:28 +0200

    desc

--- database/migrations/2025_09_02_111245_create_artists_table.php
+++ database/migrations/2025_09_02_111245_create_artists_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('nationality');
             $table->string('image')->nullable();
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->string('is_band');
         });
     }
